9c9b6cefee985f4a0d0d3795c9bc633927a59b9f
David Rodriguez <davrdz25@yahoo.com.mx>
2023-02-22 19:09:34 -0600

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,26 @@ float distanceCm;
 bool relayState;
 
 String message = "";
-const char* ssid = "Xiaomi_7D23";
-const char* password = "1234567890";
-/* const char* ssid = "INFINITUM01B6_2.4";
-const char* password = "Tp6Cy6Us1r"; */
-const char* hostname = "ESP32Server";
+/* const char* ssid = "Xiaomi_7D23";
+const char* password = "1234567890"; */
+const char *ssid = "INFINITUM01B6_2.4";
+const char *password = "Tp6Cy6Us1r";
+const char *hostname = "ESP32Server";
 
+const int waitTime = 15;
 const int relayPin = 21;
 const int echoPin = 22;
 const int triggPin = 23;
+
+void GetDistance()
+{
+  digitalWrite(triggPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_SPEED / 2;
+}
 
 String GetSensorValues()
 {
@@ -37,23 +48,28 @@ String GetSensorValues()
   return jsonString;
 }
 
-void initFS() {
-  if (!SPIFFS.begin()) {
+void initFS()
+{
+  if (!SPIFFS.begin())
+  {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
-  else{
-   Serial.println("SPIFFS mounted successfully");
+  else
+  {
+    Serial.println("SPIFFS mounted successfully");
   }
 }
 
-void initWiFi() {
+void initWiFi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   WiFi.hostname(hostname);
 
   Serial.print("Connecting to WiFi");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print('.');
     delay(1000);
   }
@@ -64,95 +80,100 @@ void initWiFi() {
   Serial.println(WiFi.getHostname());
 }
 
-void notifyClients(String sliderValues) {
+void notifyClients(String sliderValues)
+{
   ws.textAll(sliderValues);
 }
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
     data[len] = 0;
-    message = (char*)data;
-  
-    if (message.indexOf("WD") >= 0) {
+    message = (char *)data;
+
+    if (message.indexOf("WD") >= 0)
+    {
       notifyClients(GetSensorValues());
     }
 
-    if (message.indexOf("RS") >= 0) {
+    if (message.indexOf("RS") >= 0)
+    {
       notifyClients(GetSensorValues());
     }
 
-    if (strcmp((char*)data, "getValues") == 0) {
+    if (strcmp((char *)data, "getValues") == 0)
+    {
       notifyClients(GetSensorValues());
     }
   }
 }
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
   }
 }
 
-void initWebSocket() {
+void initWebSocket()
+{
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(relayPin, OUTPUT);
-  pinMode(triggPin, OUTPUT); 
+  pinMode(triggPin, OUTPUT);
   pinMode(echoPin, INPUT);
   digitalWrite(relayPin, LOW);
-  
-  /* initFS();
+
+  initFS();
   initWiFi();
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-  
-  AsyncElegantOTA.begin(&server,"admin","Abraham456..");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", "text/html"); });
+
+  AsyncElegantOTA.begin(&server, "admin", "Abraham456..");
   server.serveStatic("/", SPIFFS, "/");
 
   initWebSocket();
-  server.begin(); */
-
+  server.begin();
 }
 
-void loop() {
-  digitalWrite(triggPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggPin, LOW);
-  
-  duration = pulseIn(echoPin, HIGH);
-  distanceCm = duration * SOUND_SPEED/2;
-
+void loop()
+{
   Serial.println(distanceCm);
 
-  if(distanceCm > 20)
+  GetDistance();
+
+  if (distanceCm > 20)
+  {
+    while (distanceCm > 20)
+    {
+      digitalWrite(relayPin, LOW);
+    }
+  }
+  else 
   {
     digitalWrite(relayPin, HIGH);
-    //relayState = true;
   }
-  else
-  { 
-    digitalWrite(relayPin, LOW);
-    //relayState = false;
-  }
-  delay(500);
-  //ws.cleanupClients();
+
+  delay(1000);
+  // ws.cleanupClients();
 }
