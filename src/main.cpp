ccc1b9e5adf769783dd13225e29ecf5989f132bd
David Rodriguez <davrdz25@yahoo.com.mx>
2023-02-27 23:52:19 -0600

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,189 @@
 #include <Arduino.h>
-#include "Ticker.h"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <AsyncElegantOTA.h>
+#include "SPIFFS.h"
+#include <Arduino_JSON.h>
+#include <Ticker.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-// define pins
-#define TRIG_PIN 4
-#define ECHO_PIN 5
-#define PUMP_PIN 13
+#define LED_BUILTIN 2
+#define SOUND_SPEED 0.034
 
-// define constants
+#define TRIG_PIN 23
+#define ECHO_PIN 22
+#define PUMP_PIN 21
+
 #define MAX_WATER_LEVEL 20 // in cm
 #define PUMP_ON_TIME 900000 // 15 minutes in microseconds
 #define PUMP_OFF_TIME 900000 // 15 minutes in microseconds
 #define WATER_CHECK_INTERVAL 1000 // in milliseconds
 
-// define task handles
 TaskHandle_t waterLevelTaskHandle;
 TaskHandle_t pumpOnTaskHandle;
 TaskHandle_t pumpOffTaskHandle;
 
-// define ticker for water level check
 Ticker waterLevelTicker;
 
+void getWaterLevel();
 void pumpOffTask(void* parameter);
 void pumpOnTask(void* parameter);
 
-// function to get water level
+AsyncWebServer server(8081);
+AsyncWebSocket ws("/ws");
+JSONVar sensorValues;
+
+long duration;
+float distanceCm;
+bool relayState;
+
+String message = "";
+const char *ssid = "Xiaomi_7D23";
+const char *password = "1234567890";
+/* const char *ssid = "INFINITUM01B6_2.4";
+const char *password = "Tp6Cy6Us1r"; */
+const char *hostname = "ESP32Server";
+
+String GetSensorValues()
+{
+  sensorValues["WaterDistance"] = String(distanceCm);
+  sensorValues["RelayState"] = relayState;
+
+  String jsonString = JSON.stringify(sensorValues);
+  Serial.println(sensorValues);
+
+  return jsonString;
+}
+
+void initFS()
+{
+  if (!SPIFFS.begin())
+  {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  else
+  {
+    Serial.println("SPIFFS mounted successfully");
+  }
+}
+
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.hostname(hostname);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.printf("\nConnected to %s \n", &ssid);
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.getHostname());
+}
+
+void notifyClients(String sliderValues)
+{
+  ws.textAll(sliderValues);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
+    data[len] = 0;
+    message = (char *)data;
+
+    if (message.indexOf("WD") >= 0)
+    {
+      notifyClients(GetSensorValues());
+    }
+
+    if (message.indexOf("RS") >= 0)
+    {
+      notifyClients(GetSensorValues());
+    }
+
+    if (strcmp((char *)data, "getValues") == 0)
+    {
+      notifyClients(GetSensorValues());
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
+  }
+}
+
+void initWebSocket()
+{
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  digitalWrite(ECHO_PIN, LOW);
+
+  /* initFS();
+  initWiFi();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", "text/html"); });
+
+  AsyncElegantOTA.begin(&server, "admin", "Abraham456..");
+  server.serveStatic("/", SPIFFS, "/");
+ */
+/* 
+  initWebSocket();
+  server.begin(); */
+  waterLevelTicker.attach_ms(WATER_CHECK_INTERVAL, getWaterLevel);
+}
+
+void loop()
+{
+  Serial.printf("Distance: %f \n", distanceCm);
+  ws.cleanupClients();
+}
+
+void pumpOnTask(void* parameter) {
+  digitalWrite(PUMP_PIN, HIGH);
+  xTaskCreate(pumpOffTask, "PumpOffTask", 2048, NULL, 1, &pumpOffTaskHandle);
+  vTaskDelete(pumpOnTaskHandle);
+}
+
+void pumpOffTask(void* parameter) {
+  digitalWrite(PUMP_PIN, LOW);
+  xTaskCreate(pumpOnTask, "PumpOnTask", 2048, NULL, 1, &pumpOnTaskHandle);
+  vTaskDelete(pumpOffTaskHandle);
+}
+
 void getWaterLevel() {
   while (true) {
     // measure distance using HC-SR04
@@ -49,40 +207,4 @@ void getWaterLevel() {
     // wait for next water level check
     vTaskDelay(pdMS_TO_TICKS(WATER_CHECK_INTERVAL));
   }
-}
-
-void setup() {
-  int arg = 0;
-  // initialize serial communication
-  Serial.begin(115200);
-  // set pin modes
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(PUMP_PIN, OUTPUT);
-  // start water level checking using ticker
-  waterLevelTicker.attach_ms(WATER_CHECK_INTERVAL, getWaterLevel);
-}
-
-void loop() {
-  // do nothing
-}
-
-// function to turn on the water pump
-void pumpOnTask(void* parameter) {
-  // turn on the pump
-  digitalWrite(PUMP_PIN, HIGH);
-  // create task to turn off the pump after the pump-on time
-  xTaskCreate(pumpOffTask, "PumpOffTask", 2048, NULL, 1, &pumpOffTaskHandle);
-  // delete this task
-  vTaskDelete(pumpOnTaskHandle);
-}
-
-// function to turn off the water pump
-void pumpOffTask(void* parameter) {
-  // turn off the pump
-  digitalWrite(PUMP_PIN, LOW);
-  // create task to turn on the pump after the pump-off time
-  xTaskCreate(pumpOnTask, "PumpOnTask", 2048, NULL, 1, &pumpOnTaskHandle);
-  // delete this task
-  vTaskDelete(pumpOffTaskHandle);
 }
